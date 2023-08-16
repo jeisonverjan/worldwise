78e8b0a3a8a5550cd413d5995623ecdeb3e4580b
Verjan <jeisonverjan@gmail.com>
2023-08-16 17:48:11 -0500

    .

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,11 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const apiKey = process.env.VITE_REACT_APP_API_KEY;
-const authDomain = process.env.VITE_REACT_APP_AUTH_DOMAIN;
+const authDomain = import.meta.env.VITE_REACT_APP_AUTH_DOMAIN;
 const projectId = process.env.VITE_REACT_APP_PROJECT_ID;
 const storageBucket = process.env.VITE_REACT_APP_STORAGE_BUCKET;
 const messagingSenderId = process.env.VITE_REACT_APP_MESSAGING_SENDER_ID;
 const appId = process.env.VITE_REACT_APP_APP_ID;
+
+console.log(apiKey, "apiKey");
+console.log(authDomain, "AuthDomain");
 
 const firebaseConfig = {
   apiKey: apiKey,
